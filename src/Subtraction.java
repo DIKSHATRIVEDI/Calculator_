b7e6ce72cc0d2a27e59529fe75b4bdb571f79749
Diksha Trivedi <diksha.trivedi2122@gmail.com>
2025-01-17 22:06:19 +0530

    Subtraction is done

---
+++ src/Subtraction.java
@@ -0,0 +1,9 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int result = a - b - c;
+        System.out.println(result);
+    }
+}
